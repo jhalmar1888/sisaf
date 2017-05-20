633c0a81e9b7747ef184828d816f9c9d76c8aa58
Jhalmar <jhalmar1888@gmail.com>
2017-05-20 12:50:14 -0400

    ultima ACTUALIZACION

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    SISAF
                 </div>
 
                 <div class="links">
