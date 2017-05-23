4406839bd403a5cbb2fadc91bbebb836c0c2a01f
Jhalmar <jhalmar1888@gmail.com>
2017-05-23 09:39:00 -0400

    ultima ACTUALIZACION prueba con login 2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,199 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+        <!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8" />
+    <title>Inicio de Sesion</title>
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+    <meta content="" name="description" />
+    <meta content="" name="author" />
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+    <!-- ================== BEGIN BASE CSS STYLE ================== -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <link href="plugins/jquery-ui/themes/base/minified/jquery-ui.min.css" rel="stylesheet" />
+    <link href="plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="css/animate.min.css" rel="stylesheet" />
+    <link href="css/style.min.css" rel="stylesheet" />
+    <link href="css/style-responsive.min.css" rel="stylesheet" />
+    <link href="css/theme/default.css" rel="stylesheet" id="theme" />
+    <!-- ================== END BASE CSS STYLE ================== -->
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+    <!-- ================== BEGIN BASE JS ================== -->
+    <script src="plugins/pace/pace.min.js"></script>
+    <!-- ================== END BASE JS ================== -->
+</head>
+<body class="pace-top bg-white">
+<!-- begin #page-loader -->
+<div id="page-loader" class="fade in"><span class="spinner"></span></div>
+<!-- end #page-loader -->
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+<!-- begin #page-container -->
+<div id="page-container" class="fade">
+    <!-- begin login -->
+    <div class="login login-with-news-feed">
+        <!-- begin news-feed -->
+        <div class="news-feed">
+            <div class="news-image">
+                <img src="img/emi.jpeg" data-id="login-cover-image" alt="" />
+            </div>
+            <div class="news-caption">
+                <h4 class="caption-title"><i class="fa fa-diamond text-success"></i> Escuela Militar de Ingenieria</h4>
+                <p>
+                    SISAF
+                </p>
+            </div>
+        </div>
+        <!-- end news-feed -->
+        <!-- begin right-content -->
+        <div class="right-content">
+            <!-- begin login-header -->
+            <div class="login-header">
+                <div class="brand">
+                    {{--<span class="logo"></span>--}} Sistema de Administracion Financiera
+                    <small>Escuela Militar de Ingenieria</small>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-sign-in"></i>
+                </div>
+            </div>
+            <!-- end login-header -->
+            <!-- begin login-content -->
+            <div class="login-content">
+                <form action="index.html" method="POST" class="margin-bottom-0">
+                    <div class="form-group m-b-15">
+                        <input type="text" class="form-control input-lg" placeholder="Usuario" required />
+                    </div>
+                    <div class="form-group m-b-15">
+                        <input type="password" class="form-control input-lg" placeholder="Contrasena" required />
+                    </div>
+                    {{--<div class="checkbox m-b-30">--}}
+                    {{--<label>--}}
+                    {{--<input type="checkbox" /> Remember Me--}}
+                    {{--</label>--}}
+                    {{--</div>--}}
+                    <div class="login-buttons">
+                        <button type="submit" class="btn btn-success btn-block btn-lg">Iniciar Sesion</button>
+                    </div>
+                    {{--<div class="m-t-20 m-b-40 p-b-40 text-inverse">--}}
+                    {{--Not a member yet? Click <a href="register_v3.html" class="text-success">here</a> to register.--}}
+                    {{--</div>--}}
+                    <hr />
+                    <p class="text-center">
+                        &copy; EMI All Reserved 2017
+                    </p>
+                </form>
+            </div>
+            <!-- end login-content -->
+        </div>
+        <!-- end right-container -->
+    </div>
+    <!-- end login -->
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+    <!-- begin theme-panel -->
+    <div class="theme-panel">
+        <a href="javascript:;" data-click="theme-panel-expand" class="theme-collapse-btn"><i class="fa fa-cog"></i></a>
+        <div class="theme-panel-content">
+            <h5 class="m-t-0">Color Theme</h5>
+            <ul class="theme-list clearfix">
+                <li class="active"><a href="javascript:;" class="bg-green" data-theme="default" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Default">&nbsp;</a></li>
+                <li><a href="javascript:;" class="bg-red" data-theme="red" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Red">&nbsp;</a></li>
+                <li><a href="javascript:;" class="bg-blue" data-theme="blue" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Blue">&nbsp;</a></li>
+                <li><a href="javascript:;" class="bg-purple" data-theme="purple" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Purple">&nbsp;</a></li>
+                <li><a href="javascript:;" class="bg-orange" data-theme="orange" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Orange">&nbsp;</a></li>
+                <li><a href="javascript:;" class="bg-black" data-theme="black" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Black">&nbsp;</a></li>
+            </ul>
+            <div class="divider"></div>
+            <div class="row m-t-10">
+                <div class="col-md-5 control-label double-line">Header Styling</div>
+                <div class="col-md-7">
+                    <select name="header-styling" class="form-control input-sm">
+                        <option value="1">default</option>
+                        <option value="2">inverse</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row m-t-10">
+                <div class="col-md-5 control-label">Header</div>
+                <div class="col-md-7">
+                    <select name="header-fixed" class="form-control input-sm">
+                        <option value="1">fixed</option>
+                        <option value="2">default</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row m-t-10">
+                <div class="col-md-5 control-label double-line">Sidebar Styling</div>
+                <div class="col-md-7">
+                    <select name="sidebar-styling" class="form-control input-sm">
+                        <option value="1">default</option>
+                        <option value="2">grid</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row m-t-10">
+                <div class="col-md-5 control-label">Sidebar</div>
+                <div class="col-md-7">
+                    <select name="sidebar-fixed" class="form-control input-sm">
+                        <option value="1">fixed</option>
+                        <option value="2">default</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row m-t-10">
+                <div class="col-md-5 control-label double-line">Sidebar Gradient</div>
+                <div class="col-md-7">
+                    <select name="content-gradient" class="form-control input-sm">
+                        <option value="1">disabled</option>
+                        <option value="2">enabled</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row m-t-10">
+                <div class="col-md-5 control-label double-line">Content Styling</div>
+                <div class="col-md-7">
+                    <select name="content-styling" class="form-control input-sm">
+                        <option value="1">default</option>
+                        <option value="2">black</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row m-t-10">
+                <div class="col-md-12">
+                    <a href="#" class="btn btn-inverse btn-block btn-sm" data-click="reset-local-storage"><i class="fa fa-refresh m-r-3"></i> Reset Local Storage</a>
                 </div>
             </div>
         </div>
     </div>
+    <!-- end theme-panel -->
 </div>
-@endsection
+<!-- end page container -->
+
+<!-- ================== BEGIN BASE JS ================== -->
+<script src="plugins/jquery/jquery-1.9.1.min.js"></script>
+<script src="plugins/jquery/jquery-migrate-1.1.0.min.js"></script>
+<script src="plugins/jquery-ui/ui/minified/jquery-ui.min.js"></script>
+<script src="plugins/bootstrap/js/bootstrap.min.js"></script>
+<!--[if lt IE 9]>
+<script src="crossbrowserjs/html5shiv.js"></script>
+<script src="crossbrowserjs/respond.min.js"></script>
+<script src="crossbrowserjs/excanvas.min.js"></script>
+<![endif]-->
+<script src="plugins/slimscroll/jquery.slimscroll.min.js"></script>
+<script src="plugins/jquery-cookie/jquery.cookie.js"></script>
+<!-- ================== END BASE JS ================== -->
+
+<!-- ================== BEGIN PAGE LEVEL JS ================== -->
+<script src="js/apps.min.js"></script>
+<!-- ================== END PAGE LEVEL JS ================== -->
+
+<script>
+    $(document).ready(function() {
+        App.init();
+    });
+</script>
+</body>
+</html>
