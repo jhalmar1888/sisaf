39a60cc091463f561138660366051b57a058b0da
charlie <charlie00788@hotmail.com>
2017-05-23 15:07:25 -0400

    Modificados sin afectar sistema: Charlie

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| Rutas API
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Routas de la pagina Web
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
