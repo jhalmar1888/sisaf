b277b4e5aec396e5a4fd2303cda5a599bf9e94d7
Jhalmar <jhalmar1888@gmail.com>
2017-05-23 15:26:29 -0400

    ultima ACTUALIZACION prueba ramos

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Routas de la pagina Web
+| Routas de la pagina Web finaliz ramos
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
